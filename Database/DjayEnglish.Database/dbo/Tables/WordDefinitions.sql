df25ae2083bd0b34ba7830a6c7774bf01857e369
maarraa <maarraa@mail.ru>
2021-05-21 10:35:54 +0600

    fix examples  and definitions table in database

--- Database/DjayEnglish.Database/dbo/Tables/WordDefinitions.sql
+++ Database/DjayEnglish.Database/dbo/Tables/WordDefinitions.sql
@@ -2,6 +2,9 @@
     [Id]         INT            IDENTITY (1, 1) NOT NULL PRIMARY KEY,
     [WordId]     INT            NOT NULL,
     [Definition] NVARCHAR (700) NOT NULL,
+    [IsActive]   BIT            NOT NULL DEFAULT(0),
+    [SourceId]   INT            NOT NULL DEFAULT(0),
+    [SourceName] NVARCHAR (50)  NOT NULL DEFAULT('UNKNOWN 1')
     CONSTRAINT [FK_WordDefinitionsWordId_ToWords] FOREIGN KEY ([WordId]) REFERENCES [dbo].[Words] ([Id])
 );
 
--- Database/DjayEnglish.Database/dbo/Tables/WordExamples.sql
+++ Database/DjayEnglish.Database/dbo/Tables/WordExamples.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[WordExamples] (
     [Id]               INT IDENTITY (1, 1) NOT NULL PRIMARY KEY,
     [WordDefinitionId] INT NOT NULL,
-    [Examle]           INT NOT NULL,
+    [Example]          NVARCHAR(700) NOT NULL,
     [IsActive]         BIT NOT NULL,
     CONSTRAINT [FK_WordExamplesWordDefinitionId_ToWordDefinitions] FOREIGN KEY (WordDefinitionId) REFERENCES [dbo].WordDefinitions ([Id])
 );
