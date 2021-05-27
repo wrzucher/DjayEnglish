69ccc1ad4a25a6d3b26244fa279f41cd6ac5058a
maarraa <maarraa@mail.ru>
2021-05-27 11:42:04 +0600

    Do not use nchar in db

--- Database/DjayEnglish.Database/dbo/Tables/Quizzes.sql
+++ Database/DjayEnglish.Database/dbo/Tables/Quizzes.sql
@@ -1,10 +1,10 @@
 ﻿CREATE TABLE [dbo].[Quizzes] (
     [Id]                INT                IDENTITY (1, 1) NOT NULL PRIMARY KEY,
     [WordDefinitionId]  INT                NOT NULL,
-    [Question]          NCHAR (250)        NOT NULL,
+    [Question]          NVARCHAR (250)     NOT NULL,
     [IsActive]          BIT                NOT NULL,
     [Created]           DATETIMEOFFSET (7) NOT NULL,
     [Closed]            DATETIMEOFFSET (7) NULL,
+    [FileName]          NVARCHAR (50)      NULL,
     CONSTRAINT [FK_QuizzesWordDefinitionId_ToWordDefinitions] FOREIGN KEY (WordDefinitionId) REFERENCES [dbo].WordDefinitions ([Id])
 );
-
